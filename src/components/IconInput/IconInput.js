21eb049423340980b05ed309c88569cdbb34a2ee
Ryan Hansen <rphansen91@gmail.com>
2022-11-28 11:29:16 -0800

    stroke width

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,77 +1,83 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
 const STYLES = {
   small: {
-    '--font-size': `${14 / 16}rem`,
-    '--padding': '4px 24px'
+    fontSize: `${14 / 16}rem`,
+    padding: "4px 24px",
+    strokeWidth: 1,
+    size: 16,
   },
   large: {
-    '--font-size': `${18 / 16}rem`,
-    '--padding': '7px 36px'
+    fontSize: `${18 / 16}rem`,
+    padding: "7px 36px",
+    strokeWidth: 2,
+    size: 18,
   },
-}
-
-const ICONS_SIZE = {
-  small: 16,
-  large: 24,
-}
-
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return <Wrapper width={width}>
-    <VisuallyHidden>{label}</VisuallyHidden>
-    <IconPlacement id={icon} size={ICONS_SIZE[size]} />
-    <Input placeholder={placeholder} style={STYLES[size]} />
-  </Wrapper>;
 };
 
-const toUnit = (unit) => (props) => typeof props[unit] === 'number' ? `${props[unit]}px` : props[unit]
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  const styles = STYLES[size];
+  return (
+    <Wrapper
+      style={{
+        "--stroke-width": styles["strokeWidth"] + "px",
+        width
+      }}
+    >
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconPlacement>
+        <Icon id={icon} size={styles.size} strokeWidth={styles.strokeWidth} />
+      </IconPlacement>
+      <Input
+        placeholder={placeholder}
+        style={{
+          "--font-size": styles.fontSize,
+          "--padding": styles.padding,
+        }}
+      />
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.label`
-position: relative;
-display: inline-block;
-border-bottom: 1px solid;
-width: ${toUnit('width')};
-color: ${COLORS.gray700};
+  position: relative;
+  display: inline-block;
+  border-bottom: var(--stroke-width) solid;
+  color: ${COLORS.gray700};
 
-&:hover {
-  color: ${COLORS.black};
-}
-`
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
 
-const IconPlacement = styled(Icon)`
-position: absolute;
-margin: auto;
-left: 0;
-top: 50%;
-transform: translateY(-50%);
-`
+const IconPlacement = styled.div`
+  position: absolute;
+  margin: auto;
+  left: 0;
+  top: 50%;
+  transform: translateY(-50%);
+`;
 
 const Input = styled.input`
-padding: var(--padding);
-font-size: var(--font-size);
-width: 100%;
-border: 0;
-outline-offset: 4px;
-color: inherit;
-background-color: transparent;
-font-weight: 700;
+  padding: var(--padding);
+  font-size: var(--font-size);
+  width: 100%;
+  border: 0;
+  outline-offset: 4px;
+  color: inherit;
+  background-color: transparent;
+  font-weight: 700;
 
-&::placeholder {
-  color: ${COLORS.gray500};
-  font-weight: normal;
-}
-`
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: normal;
+  }
+`;
 
 export default IconInput;
